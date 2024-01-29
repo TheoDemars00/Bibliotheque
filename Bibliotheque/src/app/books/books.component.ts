56bed05accd1b4375cba4eef04b25485409a5614
TheoDemars00 <tdemars@genesienne.fr>
2024-01-29 17:55:21 +0100

    comminting

---
+++ Bibliotheque/src/app/books/books.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import * as books from '../../ressource/books.json';
+
+@Component({
+  selector: 'app-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent {
+
+}
