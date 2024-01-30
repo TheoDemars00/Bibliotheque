f696240391d03829077963a1ffa3983af9d3314c
TheoDemars00 <tdemars@genesienne.fr>
2024-01-30 17:20:04 +0100

    commiting

--- Bibliotheque/src/app/books/books.component.ts
+++ Bibliotheque/src/app/books/books.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import * as books from '../../ressource/books.json';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,NgFor],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent {
-
+  bookList = (books as any).default;
 }
