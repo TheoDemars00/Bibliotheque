d015227f80c5d2ab58a82e209b0d7f191a7d6549
TheoDemars00 <tdemars@genesienne.fr>
2024-02-08 11:09:19 +0100

    commiting on main the base of the projet

---
+++ bibliotheque/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}",],
+  theme: {
+    colors: {
+      'background1': '#EFF5F5',
+      'grey1': '#D6E4E5',
+      'grey2': '#497174',
+      'orange1': '#EB6440',
+      'green1' : '#386641',
+      'white': '#FFFFFF',
+      'blue': '#188FA7',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
